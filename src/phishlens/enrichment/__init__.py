a9c27d44084e3d57932570f48b38a274e64688a1
SadashivPole <sadashivpole1@gmail.com>
2026-09-22 00:08:34 +0530

    Add AbuseIPDB threat intelligence adapter

--- src/phishlens/enrichment/__init__.py
+++ src/phishlens/enrichment/__init__.py
@@ -1,3 +1,4 @@
+from .abuseipdb import AbuseIPDBProvider
 from .base import ThreatIntelProvider
 from .configured import ConfiguredThreatIntelProvider, providers_from_config
 from .mock_provider import MockThreatIntelProvider
@@ -5,6 +6,7 @@ from .virustotal import VirusTotalProvider
 from .orchestrator import EnrichmentOrchestrator
 
 __all__ = [
+    "AbuseIPDBProvider",
     "ConfiguredThreatIntelProvider",
     "EnrichmentOrchestrator",
     "MockThreatIntelProvider",
